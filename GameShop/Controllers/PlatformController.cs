9301829982c349d03648973f7cc5bd8e9ebfdb76
lanzen <50164202+Lanzen-Zero@users.noreply.github.com>
2023-07-10 22:10:45 +0900

    Platform情報取得

--- GameShop/Controllers/PlatformController.cs
+++ GameShop/Controllers/PlatformController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GameShop.DataAccess.Data;
+using GameShop.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GameShopWeb.Controllers
 {
     public class PlatformController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public PlatformController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            //Platform情報取得
+            List<Platform> objPlatformList = _db.Platforms.ToList();
             return View();
         }
     }
