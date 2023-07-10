a8bfb332b0fbc7ed77e4a53210fe99dcdc6022d8
lanzen <50164202+Lanzen-Zero@users.noreply.github.com>
2023-07-10 18:16:03 +0900

    Add project files.

---
+++ GameShop.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using GameShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameShop.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
