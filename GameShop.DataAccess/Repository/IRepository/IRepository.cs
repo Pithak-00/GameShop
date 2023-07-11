29292237740398ff1fbd475a5f25d5ab040a714e
lanzen <50164202+Lanzen-Zero@users.noreply.github.com>
2023-07-11 13:15:29 +0900

    リポジトリパターン - Interface作成

---
+++ GameShop.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace GameShop.DataAccess.Repository.IRepository
+{
+	internal interface IRepository<T> where T : class
+	{
+		//T - Platform
+		IEnumerable<T> GetAll();
+		T Get(Expression<Func<T, bool>> filter);
+		void Add(T entity);
+		void Remove(T entity);
+		void RemoveRange(IEnumerable<T> entity);
+	}
+}
