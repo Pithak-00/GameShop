f5b60f497d2a79be682b3ed1448c9ec18edfa59a
lanzen <50164202+Lanzen-Zero@users.noreply.github.com>
2023-07-11 14:46:33 +0900

    リポジトリ - UnitOfWork

--- GameShop.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ GameShop.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -1,5 +1,4 @@
-﻿using GameShop.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace GameShop.DataAccess.Repository.IRepository
 {
-	public interface IPlatformRepository : IRepository<Platform>
+	public interface IUnitOfWork
 	{
-		void Update(Platform obj);
+		IPlatformRepository Platform { get; }
+		void Save();
 	}
 }
--- GameShop.DataAccess/Repository/UnitOfWork.cs
+++ GameShop.DataAccess/Repository/UnitOfWork.cs
@@ -1,25 +1,27 @@
 ﻿using GameShop.DataAccess.Data;
 using GameShop.DataAccess.Repository.IRepository;
-using GameShop.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GameShop.DataAccess.Repository
 {
-	public class PlatformRepository : Repository<Platform>, IPlatformRepository
+	public class UnitOfWork : IUnitOfWork
 	{
 		ApplicationDbContext _db;
-		public PlatformRepository(ApplicationDbContext db) : base(db)
+		public IPlatformRepository Platform { get; private set; }
+		public UnitOfWork(ApplicationDbContext db)
 		{
 			_db = db;
+			Platform = new PlatformRepository(_db);
 		}
-		public void Update(Platform obj)
+		
+
+		public void Save()
 		{
-			_db.Update(obj);
+			_db.SaveChanges();
 		}
 	}
 }
