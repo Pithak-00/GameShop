1b7eab9bb0b229f195ccea0f8941c288cd307c47
lanzen <50164202+Lanzen-Zero@users.noreply.github.com>
2023-07-11 13:51:08 +0900

    リポジトリ - PlatformRepositoryのIntrface実装

---
+++ GameShop.DataAccess/Repository/PlatformRepository.cs
@@ -0,0 +1,30 @@
+﻿using GameShop.DataAccess.Data;
+using GameShop.DataAccess.Repository.IRepository;
+using GameShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShop.DataAccess.Repository
+{
+	public class PlatformRepository : Repository<Platform>, IPlatformRepository
+	{
+		ApplicationDbContext _db;
+		public PlatformRepository(ApplicationDbContext db) : base(db)
+		{
+			_db = db;
+		}
+		public void Save()
+		{
+			_db.SaveChanges();
+		}
+
+		public void Update(Platform obj)
+		{
+			_db.Update(obj);
+		}
+	}
+}
