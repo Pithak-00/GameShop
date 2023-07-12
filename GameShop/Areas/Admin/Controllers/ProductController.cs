51be4403ca2250367ed30f795f3a3169ce0b06f9
lanzen <50164202+Lanzen-Zero@users.noreply.github.com>
2023-07-13 01:01:27 +0900

    DataTables API 追加

--- GameShop/Areas/Admin/Controllers/ProductController.cs
+++ GameShop/Areas/Admin/Controllers/ProductController.cs
@@ -152,5 +152,14 @@ namespace GameShopWeb.Areas.Admin.Controllers
             TempData["success"] = "Product deleted successfully";
             return RedirectToAction("Index");
         }
-    }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Platform").ToList();
+            return Json(new { data = objProductList });
+        }
+		#endregion
+	}
 }
