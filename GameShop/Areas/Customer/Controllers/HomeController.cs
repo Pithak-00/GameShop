02dc88472e2502be364dd73125411972b98c359a
lanzen <50164202+Lanzen-Zero@users.noreply.github.com>
2023-07-13 16:33:57 +0900

    ホームページ編集

--- GameShop/Areas/Customer/Controllers/HomeController.cs
+++ GameShop/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using GameShop.Models;
+﻿using GameShop.DataAccess.Repository.IRepository;
+using GameShop.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,15 +9,18 @@ namespace GameShopWeb.Areas.Customer.Controllers
 	public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Platform");
+            return View(productList);
         }
 
         public IActionResult Privacy()
