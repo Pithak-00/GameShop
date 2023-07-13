a397b9f8d0bc9f897674c3336bc73a8bbd9ce2ba
lanzen <50164202+Lanzen-Zero@users.noreply.github.com>
2023-07-13 16:03:39 +0900

    削除機能追加

--- GameShop/wwwroot/js/product.js
+++ GameShop/wwwroot/js/product.js
@@ -3,14 +3,21 @@ $(document).ready(function () {
 });
 
 function loadDataTable() {
-    dataTable = $('#tblData').DataTable({
-        "ajax": { url: '/admin/product/getall' },
-        "columns": [
-            { data: 'title', "width": "15%" },
-            { data: 'brand', "width": "15%" },
-            { data: 'platform.name', "width": "10%" },
-            { data: 'listPrice', "width": "10%" },
-            { data: 'description', "width": "25%" }
-        ]
-    });
+	dataTable = $('#tblData').DataTable({
+		"ajax": { url: '/admin/product/getall' },
+		"columns": [
+			{ data: 'title', "width": "15%" },
+			{ data: 'brand', "width": "10%" },
+			{ data: 'platform.name', "width": "5%" },
+			{ data: 'listPrice', "width": "5%" },
+			{ data: 'description', "width": "40%" },
+			{
+				data: 'id',
+				"render": function (data) {
+					return '<div class="w-75 btn-group" role="group"><a herf = "/admin/product/upsert?id=${data}" class="btn btn-primary mx-2" ><i class="bi bi-pencil-square" > </i> Edit </a><a href="/admin/delete/${data}" class="btn btn-danger mx-2" ><i class="bi bi-trash-fill" > </i> Delete </a></div>'
+				},
+				"width": "15%"
+			}
+		]
+	});
 }
