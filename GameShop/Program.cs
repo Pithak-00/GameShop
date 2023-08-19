3e946babf53f1a636964ef19d2aeaaeb24d4c0d7
Pithak-00 <pithak1991@gmail.com>
2023-08-19 19:30:39 +0900

    権限をデータベースに追加

--- GameShop/Program.cs
+++ GameShop/Program.cs
@@ -4,6 +4,8 @@ using GameShop.DataAccess.Repository;
 using GameShop.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using GameShop.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,9 +14,11 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
+
 builder.Services.AddApplicationInsightsTelemetry();
 
 var app = builder.Build();
