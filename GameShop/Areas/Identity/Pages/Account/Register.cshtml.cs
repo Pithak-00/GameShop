622668fc5c4a127d169e076a5af5627a53ede3b6
Pithak-00 <pithak1991@gmail.com>
2023-08-19 18:53:42 +0900

    ApplicationUsersとして新規申請

--- GameShop/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ GameShop/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using GameShop.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -154,11 +155,11 @@ namespace GameShopWeb.Areas.Identity.Pages.Account
             return Page();
         }
 
-        private IdentityUser CreateUser()
+        private ApplicationUsers CreateUser()
         {
             try
             {
-                return Activator.CreateInstance<IdentityUser>();
+                return Activator.CreateInstance<ApplicationUsers>();
             }
             catch
             {
