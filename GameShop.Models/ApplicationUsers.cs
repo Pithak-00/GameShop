8a65137be6d61ae5f23245afd9297e9367f6bb8b
Pithak-00 <pithak1991@gmail.com>
2023-08-22 14:32:32 +0900

    申請情報追加

--- GameShop.Models/ApplicationUsers.cs
+++ GameShop.Models/ApplicationUsers.cs
@@ -11,7 +11,7 @@ namespace GameShop.Models
 	public class ApplicationUsers:IdentityUser
 	{
 		[Required]
-		public int Name { get; set; }
+		public string Name { get; set; }
 
 		public string? StreetAddress { get; set; }
 		public string? City { get; set; }
