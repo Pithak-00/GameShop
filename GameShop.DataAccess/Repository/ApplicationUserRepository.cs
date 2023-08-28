7789a5a61485f962bd0ac0157c25039a2700c0f5
Pithak-00 <pithak1991@gmail.com>
2023-08-28 13:37:40 +0900

    ApplicationUserをRepositoryに追加

---
+++ GameShop.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using GameShop.DataAccess.Data;
+using GameShop.DataAccess.Repository.IRepository;
+using GameShop.Models;
+
+namespace GameShop.DataAccess.Repository
+{
+	public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+		ApplicationDbContext _db;
+		public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+		{
+			_db = db;
+		}
+	}
+}
---
+++ GameShop.DataAccess/Repository/IRepository/IApplicationUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using GameShop.Models;
+
+namespace GameShop.DataAccess.Repository.IRepository
+{
+	public interface IApplicationUserRepository : IRepository<ApplicationUser>
+	{
+	}
+}
