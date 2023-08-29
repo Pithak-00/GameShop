e36e8b56d6cc8a72edb68ee29adea33358d49895
Pithak-00 <pithak1991@gmail.com>
2023-08-29 16:45:01 +0900

    注文ステータス

--- GameShop.Utility/SD.cs
+++ GameShop.Utility/SD.cs
@@ -12,5 +12,17 @@ namespace GameShop.Utility
 		public const string Role_Company = "企業";
 		public const string Role_Admin = "管理者";
 		public const string Role_Employee = "従業員";
+
+		public const string StatusPending = "Pending";
+		public const string StatusApproved = "Approved";
+		public const string StatusInProcess = "Processing";
+		public const string StatusShipped = "Shipped";
+		public const string StatusCancelled = "Cancelled";
+		public const string StatusRefunded = "Refunded";
+
+		public const string PaymentStatusPending = "Pending";
+		public const string PaymentStatusApproved = "Approved";
+		public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+		public const string PaymentStatusRejected = "Rejected";
 	}
 }
